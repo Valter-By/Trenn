e6b6c2992676a9b45488e73a44b687411323444a
Valter-By <valter.by@gmail.com>
2020-05-08 19:39:55 +0300

    All

--- src/lesson1/Task3.java
+++ src/lesson1/Task3.java
@@ -1,6 +1,27 @@
 package lesson1;
 
+import java.math.BigDecimal;
+
 public class Task3 {
+    public static void main(String[] args) {
+
+        // BigDecimal x = new BigDecimal(1);
+
+
+
+        Event[] evs = new Event[5];
+
+        for (int i = 0; i < 5; i++) {
+                evs[i] = new Event(i + 1001, i, i, Integer.toString(i) + " name ");
+        }
+
+        //sortEvents(evs);
+
+        for (int i = 0; i < 5; i++) {
+           System.out.println(evs[i].name + " name ");
+        }
+
+    }
 
     static class Event {
 
@@ -28,9 +49,17 @@ public class Task3 {
         Event t = new Event();
 
         for (int i = 0; i < n; i++) {
-
+            for (int k = 0; k < n - i - 1; k++) {
+                if (arr[k].y > arr[k + 1].y ||
+                        arr[k].y == arr[k + 1].y && arr[k].m > arr[k + 1].m ||
+                        arr[k].y == arr[k + 1].y && arr[k].m == arr[k + 1].m  &&
+                                arr[k].d > arr[k + 1].d) {
+                    t = arr[k];
+                    arr[k] = arr[k + 1];
+                    arr[k + 1] = t;
+                }
+            }
         }
-
     }
 
 }
