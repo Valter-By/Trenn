af873ed1ad0a379f078ca3b2a07f58ad2a75420d
Valter-By <valter.by@gmail.com>
2020-05-08 15:39:24 +0300

    All

---
+++ src/lesson1/Task3.java
@@ -0,0 +1,36 @@
+package lesson1;
+
+public class Task3 {
+
+    static class Event {
+
+        static int d;
+        static int m;
+        static int y;
+        static String name;
+
+        public Event() {
+            d = 0;
+            m = 0;
+            y = 0;
+            name = null;
+        }
+        public Event(int d, int m, int y, String name) {
+            this.d = d;
+            this.m = m;
+            this.y = y;
+            this.name = name;
+        }
+    }
+
+    static void sortEvents(Event[] arr) {
+        int n = arr.length;
+        Event t = new Event();
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+    }
+
+}
