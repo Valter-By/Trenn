2af95b9722ecbf37eb822eff93e353b7ac6dca0c
Valter-By <valter.by@gmail.com>
2020-05-08 13:56:41 +0300

    All

--- src/lesson1/Task2.java
+++ src/lesson1/Task2.java
@@ -6,7 +6,22 @@ public class Task2 {
 
     }
 
-    
+    public void downRow(int[][] ar) {
+        int n = ar.length;
+        int m = ar[0].length;
+        int[] sum = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sum[i] += ar[i][j];
+
+            }
+        }
+
+
+    }
+
+
 
 
 }
